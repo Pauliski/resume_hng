9f666e1bf50afbd61a4a1e105a459cd3a42daea6
Paul <parokoola@gmail.com>
2021-08-17 17:38:43 +0100

    resume for frontend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import Form from './components/Form/Form';
-import Navbar from './components/Navbar/Navbar';
-import RessumeBody from './components/ResumeBody/ResumeBody';
-import Sidebar from './components/Sidebar/Sidebar';
+
 import Resume from './pages/Resume/Resume';
 
 function App() {
   return (
     <div className="App">
-     {/* <Navbar />
-     <Form /> */}
+     
     <Resume />
     </div>
   );
